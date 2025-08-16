b8d3d93855ffca676e0e7bee8a75f8cb655546f7
Sojib Ahmed <mdaminurislmsojib2006@gmail.com>
2025-08-16 16:21:57 +0600

    add image section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import { Header } from "./assets/components/Header";
+import logoImage from "./assets/logo.png";
+import productImage from "./assets/Untitled design.png";
 
 function App() {
   const [cartProduct, setCartProduct] = useState([]);
   
 
   const productList = [
-    { id: "01", name: "Product One" },
-    { id: "02", name: "Product Two" },
-    { id: "03", name: "Product Three" },
-    { id: "04", name: "Product Four" },
+    { id: "01", name: "Product One", image: productImage },
+    { id: "02", name: "Product Two", image: logoImage },
+    { id: "03", name: "Product Three", image: productImage },
+    { id: "04", name: "Product Four", image: logoImage },
   ];
 
   const addToCart = (product ) => {
@@ -25,6 +27,9 @@ function App() {
         <div key={product.id}>
           <div className="h-20 p-2 rounded-lg bg-amber-100 mb-2">
             <h1>{product.name}</h1>
+            {product.image && (
+              <img src={product.image} alt={product.name} className="w-16 h-16 object-cover" />
+            )}
           </div>
           <button onClick={() => addToCart(product)} className="btn">
             Add Product
